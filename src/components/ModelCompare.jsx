7cf0cb443c22979caba868fec2277e451c825df3
Younis Mubashar Raja <142894776+younisrajaApplications@users.noreply.github.com>
2025-10-13 23:25:24 +0100

    Give a more F1 look

--- src/components/ModelCompare.jsx
+++ src/components/ModelCompare.jsx
@@ -10,22 +10,30 @@ export default function ModelCompare({userPicks, modelPicks, onRefreshModel}) {
     return (
         <div className="compare-grid">
             {/* User Side */}
-            <div className="card panel panel--user" style={{ margin: 0 }}>
+            <div className="card panel panel--user f1-kerb f1-checker panel--race" style={{ margin: 0 }}>
                 <h4 className="panel-title" style={{marginBottom: 10}}>
                     <span className="badge">Your Podium</span>
                 </h4>
-                <Podium picks={userPicks} />
+                {/* Start lights above user reveal (purely visual) */}
+                <div className="start-lights">
+                    <span className="light on" />
+                    <span className="light on" />
+                    <span className="light on" />
+                    <span className="light" />
+                    <span className="light" />
+                </div>
+                <Podium picks={userPicks} model={"userBtn"} />
             </div>
 
             {/* Model Side */}
-            <div className="card panel panel--model" style={{ margin: 0 }}>
+            <div className="card panel panel--model f1-kerb f1-checker panel--race" style={{ margin: 0 }}>
                 <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: 8 }}>
                     <h4 className="panel-title" style={{ margin: 0 }}>
                         <span className="badge"> Model prediction</span>
                     </h4>
                     <button className="btn" onClick={onRefreshModel}>Refresh model</button>
                 </div>
-                <Podium picks={modelPicks}/>
+                <Podium picks={modelPicks} model={"machineBtn"}/>
             </div>
         </div>
     )
